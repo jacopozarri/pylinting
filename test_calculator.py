9c25e879817e4933c8924f0cc674d872b335b81e
Jacopo Zarri <zarri.jacopo1992@gmail.com>
2023-10-22 23:05:06 +0200

    .

--- test_calculator.py
+++ test_calculator.py
@@ -8,23 +8,31 @@ class TestCalculator(unittest.TestCase):
 
 
 
+
     def test_add(self):
         self.assertEqual(self.calc.add(0, 2), 3)
         self.assertEqual(self.calc.add(-1, -1), -2)
+
+
 
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(3, 1), 2)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
 
+
+
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(3, 3), 9)
         self.assertEqual(self.calc.multiply(-1, 3), -3)
+
+
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(6, 2), 3)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
         with self.assertRaises(ValueError):
             self.calc.divide(1, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
