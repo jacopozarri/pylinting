b66c27f71cf7e0b460d1050db8c735dfcfdd01e7
Jacopo Zarri <zarri.jacopo1992@gmail.com>
2023-10-22 23:19:40 +0200

    .

--- test_calculator.py
+++ test_calculator.py
@@ -1,31 +1,22 @@
 import unittest
 from calculator import Calculator
 
+
 class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
-
-
-
-
 
     def test_add(self):
         self.assertEqual(self.calc.add(0, 2), 3)
         self.assertEqual(self.calc.add(-1, -1), -2)
 
-
-
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(3, 1), 2)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
 
-
-
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(3, 3), 9)
         self.assertEqual(self.calc.multiply(-1, 3), -3)
-
-
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(6, 2), 3)
@@ -34,5 +25,5 @@ class TestCalculator(unittest.TestCase):
             self.calc.divide(1, 0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
