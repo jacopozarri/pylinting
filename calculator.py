7756dd14b47ccbbd3a0df3b9f07230fbbbf04082
Jacopo Zarri <zarri.jacopo1992@gmail.com>
2023-10-22 23:21:43 +0200

    .

--- calculator.py
+++ calculator.py
@@ -5,21 +5,8 @@ class Calculator:
     def subtract(self, a, b):
         return a - b
 
-
-
-
-
-
-
-
     def multiply(self, a, b):
         return a * b
-
-
-
-
-
-
 
     def divide(self, a, b):
         if b == 0:
